80c5bafa976c5af777c7e0bd9fe7bf554a33a565
Eugene Bogdanov <eugeneb1984@gmail.com>
2022-11-26 18:05:54 +0300

    Corrected syntax

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -5,7 +5,7 @@ bool isNumber = Int32.TryParse(input, out int number);
 
 if (isNumber) {
     Console.WriteLine (number % 2 == 0 ? "Это чётное число" : "Это нечётное число");
-    }
+}
 else {
     Console.WriteLine ("Похоже вы ошиблись при вводе. Убедитесь, что вводите число");
 }
