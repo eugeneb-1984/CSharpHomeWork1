2f5c4ea59c22e9346e650b227e40edbd381a7bbf
Eugene Bogdanov <eugeneb1984@gmail.com>
2022-11-26 18:04:59 +0300

    Added task 6

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+string input = Console.ReadLine();
+
+bool isNumber = Int32.TryParse(input, out int number);
+
+if (isNumber) {
+    Console.WriteLine (number % 2 == 0 ? "Это чётное число" : "Это нечётное число");
+    }
+else {
+    Console.WriteLine ("Похоже вы ошиблись при вводе. Убедитесь, что вводите число");
+}
