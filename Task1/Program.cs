6115d61fe49146ac8869c82de5aa5dfbf6e38588
Eugene Bogdanov <eugeneb1984@gmail.com>
2022-11-26 17:16:18 +0300

    Added Task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое целое число: ");
+string inputA = Console.ReadLine();
+Console.Write("Введите второе целое число: ");
+string inputB = Console.ReadLine();
+bool isNumberA = Int32.TryParse(inputA, out int numberA);
+bool isNumberB = Int32.TryParse(inputB, out int numberB);
+
+if (isNumberA && isNumberB) {
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(numberA > numberB ? numberA : numberB);
+}
+ else {
+    Console.Write("Похоже вы ввели не число. Перезапустите программу и убедитесь, что вы вводите числа.");
+ }
