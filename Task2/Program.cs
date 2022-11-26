d33f9d3465e52d721890c577632eaa76a16d277f
Eugene Bogdanov <eugeneb1984@gmail.com>
2022-11-26 17:53:58 +0300

    Renamed folders

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,21 +2,14 @@
 string inputA = Console.ReadLine();
 Console.Write("Введите второе целое число: ");
 string inputB = Console.ReadLine();
-Console.Write("Введите второе целое число: ");
-string inputC = Console.ReadLine();
 
 // парсим ввод юзера
 bool isNumberA = Int32.TryParse(inputA, out int numberA);
 bool isNumberB = Int32.TryParse(inputB, out int numberB);
-bool isNumberC = Int32.TryParse(inputC, out int numberC);
 
-if (isNumberA && isNumberB && isNumberC) { // убеждаемся, что оба ввода - это целые числа
-    int maxNumber = numberA;
-    if (numberB > maxNumber) maxNumber = numberB;
-    if (numberC > maxNumber) maxNumber = numberC;
-
+if (isNumberA && isNumberB) { // убеждаемся, что оба ввода - это целые числа
     Console.Write("Максимальное число: ");
-    Console.WriteLine(maxNumber);
+    Console.WriteLine(numberA > numberB ? numberA : numberB);
 }
  else {
     Console.Write("Похоже вы ошиблись при вводе. Перезапустите программу и убедитесь, что вы вводите числа.");
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,14 +2,21 @@
 string inputA = Console.ReadLine();
 Console.Write("Введите второе целое число: ");
 string inputB = Console.ReadLine();
+Console.Write("Введите второе целое число: ");
+string inputC = Console.ReadLine();
 
 // парсим ввод юзера
 bool isNumberA = Int32.TryParse(inputA, out int numberA);
 bool isNumberB = Int32.TryParse(inputB, out int numberB);
+bool isNumberC = Int32.TryParse(inputC, out int numberC);
 
-if (isNumberA && isNumberB) { // убеждаемся, что оба ввода - это целые числа
+if (isNumberA && isNumberB && isNumberC) { // убеждаемся, что оба ввода - это целые числа
+    int maxNumber = numberA;
+    if (numberB > maxNumber) maxNumber = numberB;
+    if (numberC > maxNumber) maxNumber = numberC;
+
     Console.Write("Максимальное число: ");
-    Console.WriteLine(numberA > numberB ? numberA : numberB);
+    Console.WriteLine(maxNumber);
 }
  else {
     Console.Write("Похоже вы ошиблись при вводе. Перезапустите программу и убедитесь, что вы вводите числа.");
