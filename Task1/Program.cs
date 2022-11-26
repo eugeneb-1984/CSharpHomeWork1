a0bb73d1a6ff45a1f4e8150ec08c96617016c25f
Eugene Bogdanov <eugeneb1984@gmail.com>
2022-11-26 17:29:55 +0300

    Added comments to task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,13 +2,15 @@
 string inputA = Console.ReadLine();
 Console.Write("Введите второе целое число: ");
 string inputB = Console.ReadLine();
+
+// парсим ввод юзера
 bool isNumberA = Int32.TryParse(inputA, out int numberA);
 bool isNumberB = Int32.TryParse(inputB, out int numberB);
 
-if (isNumberA && isNumberB) {
+if (isNumberA && isNumberB) { // убеждаемся, что оба ввода - это целые числа
     Console.Write("Максимальное число: ");
     Console.WriteLine(numberA > numberB ? numberA : numberB);
 }
  else {
-    Console.Write("Похоже вы ввели не число. Перезапустите программу и убедитесь, что вы вводите числа.");
+    Console.Write("Похоже вы ошиблись при вводе. Перезапустите программу и убедитесь, что вы вводите числа.");
  }
