fbf32ceb39f7cf0103e2a9e329bcd852b87e4c74
Eugene Bogdanov <eugeneb1984@gmail.com>
2022-11-26 19:26:23 +0300

    Added task 8

---
+++ Task8/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число: ");
+string input = Console.ReadLine();
+bool isNumber = Int32.TryParse(input, out int number);
+if (isNumber) {
+    int index = 2;
+    string result = "";
+    if (index > number) {
+        Console.Write("От 1 до " + number.ToString() + " нет чётных чисел.");
+    }
+    else {
+        while (index <= number) {
+            if (string.IsNullOrEmpty(result)) {
+                result = index.ToString();
+            } 
+            else {
+                result = result + ", " + index.ToString();
+            }
+            index += 2;
+        }
+    Console.WriteLine("Чётные числа от 1 до " + number.ToString() + ":");
+    Console.WriteLine(result);
+    }
+}
+else {
+    Console.WriteLine("Вы ошиблись при вводе. Убедитесь, что вводите целое число");
+}
